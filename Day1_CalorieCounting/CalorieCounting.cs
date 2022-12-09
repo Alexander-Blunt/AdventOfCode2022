df2301c0195c22d31f4e5dad96c6969ac1fa253d
Alexander Blunt <55701570+Alexander-Blunt@users.noreply.github.com>
2022-12-09 17:56:27 +0000

    Eyball tested ReadAllLines and renamed Day1Input

---
+++ Day1_CalorieCounting/CalorieCounting.cs
@@ -0,0 +1,14 @@
+﻿namespace Day1_CalorieCounting
+{
+    internal class CalorieCounting
+    {
+        static void Main(string[] args)
+        {
+            string fileLocation = "..\\..\\..\\Day1Input.txt";
+            foreach (string item in File.ReadAllLines(fileLocation))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
