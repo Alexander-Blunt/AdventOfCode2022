ae79b87ddb5407114ca3814fb7705579be39cf1f
Alexander Blunt <55701570+Alexander-Blunt@users.noreply.github.com>
2022-12-09 17:39:29 +0000

    Implemented solution to part 2 of day 3

--- Day3/ReorganizeRucksack/Program.cs
+++ Day3/ReorganizeRucksack/Program.cs
@@ -1,55 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 
-namespace RucksackReorganization
+namespace RucksackReorganization;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(PrioritySum.FindDuplicates(@"C:\Users\Spore\source\repos\AdventOfCode2022\Day3\ReorganizeRucksack\Day3input.txt"));
-        }
+        Console.WriteLine(Rucksack.FindDuplicates(@"C:\Users\Spore\source\repos\AdventOfCode2022\Day3\ReorganizeRucksack\Day3input.txt"));
+        Console.WriteLine(Rucksack.FindBadges(@"C:\Users\Spore\source\repos\AdventOfCode2022\Day3\ReorganizeRucksack\Day3input.txt"));
+    }
+}
+
+public class Rucksack
+{
+    private int CompartmentSize;
+    private string Compartment1;
+    private string Compartment2;
+
+    public Rucksack(string contents)
+    {
+        CompartmentSize = contents.Length / 2;
+        Compartment1 = contents.Substring(0, CompartmentSize);
+        Compartment2 = contents.Substring(CompartmentSize);
     }
 
-    public class PrioritySum
+    public static int GetPriority(char item)
     {
-        public static int FindDuplicates(string fileLocation)
+        string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int priority = 0;
+        foreach (char letter in alphabet)
         {
-            string[] rucksacks = System.IO.File.ReadAllLines(fileLocation);
-            int finalSum = 0;
-            foreach (string sack in rucksacks)
+            priority++;
+            if (letter == item)
             {
-                int compartmentSize = sack.Length / 2;
-                string compartment1 = sack.Substring(0, compartmentSize);
-                string compartment2 = sack.Substring(compartmentSize);
-                foreach (char item in compartment1)
-                {
-                    if (compartment2.Contains(item))
-                    {
-                        finalSum += GetPriority(item);
-                        break;
-                    }
-                }
+                return priority;
             }
-            return finalSum;
+        }
+        return 0;
+    }
+     public static int FindDuplicates(string fileLocation)
+     {
+        // create array of rucksacks
+        string[] input = File.ReadAllLines(fileLocation);
+        Rucksack[] sacks = new Rucksack[input.Length];
+        for (int i = 0; i < input.Length; i++)
+        {
+            sacks[i] = new Rucksack(input[i]);
         }
 
-        public static int GetPriority(char item)
+        int finalSum = 0;
+        foreach (Rucksack sack in sacks)
         {
-            string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            int priority = 0;
-            foreach (char letter in alphabet)
+            foreach (char item in sack.Compartment1)
             {
-                priority++;
-                if (letter == item)
+                if (sack.Compartment2.Contains(item))
                 {
-                    return priority;
+                    finalSum += GetPriority(item);
+                    break;
                 }
             }
-            return 0;
         }
+        return finalSum;
+     }
+    public static int FindBadges(string fileLocation)
+    {
+        string[] input = File.ReadAllLines(fileLocation);
+        int finalSum = 0;
+        for (int i = 0; i < input.Length; i+=3)
+        {
+            foreach (char item in input[i])
+            {
+                if (input[i + 1].Contains(item) && input[i + 2].Contains(item))
+                {
+                    finalSum += GetPriority(item);
+                    break;
+                }
+            }
+        }
+        return finalSum;
     }
 }
