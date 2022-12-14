4cd5483ee503ad3014be00a15aa552806e31a0e2
Alexander Blunt <55701570+Alexander-Blunt@users.noreply.github.com>
2022-12-14 11:33:51 +0000

    Correct solution implemented for day 1 calorie counting.

--- Day1_CalorieCounting/CalorieCounting.cs
+++ Day1_CalorieCounting/CalorieCounting.cs
@@ -1,16 +1,46 @@
 ﻿using System.Globalization;
+using System.Runtime.CompilerServices;
 
-namespace Day1_CalorieCounting
+namespace Day1_CalorieCounting;
+
+public class CalorieCounting
 {
-    internal class CalorieCounting
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string fileLocation = "..\\..\\..\\Day1Input.txt";
+        Console.WriteLine(FindHighestCalories(fileLocation));
+    }
+
+    // Create file stream.
+    // Read file one line at a time, adding to an array representing the total of each elf.
+    // Find highest total and return it.
+    public static int FindHighestCalories(string fileLocation)
+    {
+        List<int> totalCaloriesForEachElf = GetCaloriesForEachElf(fileLocation);
+
+        int highest = totalCaloriesForEachElf[0];
+        for (int i = 1; i < totalCaloriesForEachElf.Count; i++)
         {
-            string fileLocation = "..\\..\\..\\Day1Input.txt";
-            foreach (string item in File.ReadAllLines(fileLocation))
+            if (highest < totalCaloriesForEachElf[i]) highest = totalCaloriesForEachElf[i];
+        }
+        return highest;
+    }
+
+    public static List<int> GetCaloriesForEachElf(string fileLocation)
+    {
+        List<int> outputList = new();
+        outputList.Add(0);
+        int index = 0;
+        foreach (string line in File.ReadLines(fileLocation))
+        {
+            if (!String.IsNullOrEmpty(line)) outputList[index] += Int32.Parse(line);
+            else
             {
-                Console.WriteLine(item);
+                outputList.Add(0);
+                index++;
             }
         }
+        return outputList;
+
     }
 }
