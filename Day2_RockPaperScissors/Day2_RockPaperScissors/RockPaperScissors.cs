82384dd1ae0bf0bed9022356403637e3426284e9
Alexander Blunt <55701570+Alexander-Blunt@users.noreply.github.com>
2022-12-14 16:41:12 +0000

    Added comments to improve readability.

--- Day2_RockPaperScissors/Day2_RockPaperScissors/RockPaperScissors.cs
+++ Day2_RockPaperScissors/Day2_RockPaperScissors/RockPaperScissors.cs
@@ -21,12 +21,13 @@ public class RockPaperScissors
         return sum;
     }
 
+    // You score 1 point for X (rock), 2 points for Y (paper) and 3 points for Z (scissors)
     // Results
     // Win: A Y, B Z, C X 6 points
     // Draw: A X, B Y, C Z 3 points
     // Lose: A Z, B X, C Y 0 points
     // Would need to check for bad input.
-    public static int CalculateScoreForRound(string round)
+    public static int CalculateIncorrectScoreForRound(string round)
     {
         int yourScore = 0;
         if (round.Contains("X")) yourScore += 1;
@@ -41,6 +42,66 @@ public class RockPaperScissors
         else if (round == "A X" || round == "B Y" || round == "C Z")
         {
             yourScore += 3;
+        }
+        return yourScore;
+    }
+
+    // You lose with X, draw with Y and win with Z
+    // When you play: Rock = 1 point, Paper = 2 points, Scissors = 3 points
+    // Your oponent plays: A = Rock, B = Paper, C = Scissors
+    public static int CalculateScoreForRound(string round)
+    {
+        int yourScore = 0;
+        string theirChoice = round.Substring(0, 1);
+        // lose
+        if (round.Contains("X"))
+        {
+            switch (theirChoice)
+            {
+                case "A":
+                    yourScore += 3;
+                    break;
+                case "B":
+                    yourScore += 1;
+                    break;
+                case "C":
+                    yourScore += 2;
+                    break;
+            }
+        }
+        // draw
+        else if (round.Contains("Y"))
+        {
+            switch (theirChoice)
+            {
+                case "A":
+                    yourScore += 1;
+                    break;
+                case "B":
+                    yourScore += 2;
+                    break;
+                case "C":
+                    yourScore += 3;
+                    break;
+            }
+            yourScore += 3;
+        }
+        // win
+        else if (round.Contains("Z"))
+        {
+            switch (theirChoice)
+            {
+                case "A":
+                    yourScore += 2;
+                    break;
+                case "B":
+                    yourScore += 3;
+                    break;
+                case "C":
+                    yourScore += 1;
+                    break;
+            }
+            yourScore += 6;
         }
         return yourScore;
     }
--- Day2_RockPaperScissors/RockPaperScissorsTests/RockPaperScissorsTests.cs
+++ Day2_RockPaperScissors/RockPaperScissorsTests/RockPaperScissorsTests.cs
@@ -4,9 +4,9 @@ namespace RockPaperScissorsTests
 {
     public class RockPaperScissorsTests
     {
-        [TestCase("A Y", 8)]
+        [TestCase("A Y", 4)]
         [TestCase("B X", 1)]
-        [TestCase("C Z", 6)]
+        [TestCase("C Z", 7)]
         public void GivenValidInput_CalculateRockPaperScissorsScore_ReturnsCorrectScore(string round, int expected)
         {
             Assert.That(RockPaperScissors.CalculateScoreForRound(round), Is.EqualTo(expected));
