7315d624503a59f92106ffa83a34821104742b69
Alexander Blunt <55701570+Alexander-Blunt@users.noreply.github.com>
2022-12-20 13:29:38 +0000

    Initial commit of Day 7

---
+++ Day7_NoSpaceLeftOnDevice/FileSystemApp/FileSystem.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+
+namespace FileSystemApp;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public abstract class FileSystem
+{
+    public string Name { get; set; }
+}
+
+public class File : FileSystem
+{
+    public int Size { get; set; }
+
+    public File(string lsLine)
+    {
+        string[] sizeAndName = lsLine.Split(' ');
+        Size = int.Parse(sizeAndName[0]);
+        Name = sizeAndName[1];
+    }
+}
+
+public class Directory : FileSystem
+{
+    public List<FileSystem> Contents;
+
+    public Directory()
+    {
+        Contents = new List<FileSystem>();
+    }
+
+    public void Add(FileSystem item)
+    {
+        Contents.Add(item);
+    }
+}
---
+++ Day7_NoSpaceLeftOnDevice/FileSystemTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FileSystemTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
