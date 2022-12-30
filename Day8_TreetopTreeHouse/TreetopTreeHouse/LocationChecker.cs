f291dbc7fa240d2009cd8a78957c5aa8165c0d32
Alexander Blunt <55701570+Alexander-Blunt@users.noreply.github.com>
2022-12-30 14:15:09 +0000

    Initial Commit for Day 8

---
+++ Day8_TreetopTreeHouse/TreetopTreeHouse/LocationChecker.cs
@@ -0,0 +1,9 @@
+﻿namespace TreetopTreeHouse;
+
+internal class LocationChecker
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Day8_TreetopTreeHouse/TreetopTreeHouseTests/LocationCheckerTests.cs
@@ -0,0 +1,17 @@
+using TreetopTreeHouse;
+
+namespace TreetopTreeHouseTests;
+
+public class LocationCheckerTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
