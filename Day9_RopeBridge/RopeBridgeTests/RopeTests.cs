eb59a4fcdc32f5f86fdcc6a47edf9d6a9ba585ea
Alexander Blunt <55701570+Alexander-Blunt@users.noreply.github.com>
2023-01-04 08:16:47 +0000

    Added unit tests for moving left and right

--- Day9_RopeBridge/RopeBridgeTests/RopeTests.cs
+++ Day9_RopeBridge/RopeBridgeTests/RopeTests.cs
@@ -34,4 +34,36 @@ public class RopeTests
 
         Assert.That(output, Is.EqualTo(expected));
     }
+
+    [TestCase(2, -1, 0)]
+    [TestCase(1, 0, 0)]
+    [TestCase(3, -2, 0)]
+    public void GivenTheHeadMovesLeft_TheTail_CorrectlyFollows(int numberOfSteps, int expectedTailX, int expectedTailY)
+    {
+        int[] expected = { expectedTailX, expectedTailY };
+        Rope testRope = new();
+        for (int i = 0; i < numberOfSteps; i++)
+        {
+            testRope.MoveLeft();
+        }
+        int[] output = testRope.Tail;
+
+        Assert.That(output, Is.EqualTo(expected));
+    }
+
+    [TestCase(2, 1, 0)]
+    [TestCase(1, 0, 0)]
+    [TestCase(3, 2, 0)]
+    public void GivenTheHeadMovesRight_TheTail_CorrectlyFollows(int numberOfSteps, int expectedTailX, int expectedTailY)
+    {
+        int[] expected = { expectedTailX, expectedTailY };
+        Rope testRope = new();
+        for (int i = 0; i < numberOfSteps; i++)
+        {
+            testRope.MoveRight();
+        }
+        int[] output = testRope.Tail;
+
+        Assert.That(output, Is.EqualTo(expected));
+    }
 }
