a6a8f816bbd2e42c2ff2bdf4835a6180f369ac09
Alexander Blunt <55701570+Alexander-Blunt@users.noreply.github.com>
2023-01-13 20:04:15 +0000

    Implemented a correct solution to day 10 part 1

--- Day10_CathodeRayTube/CathodeRayTube/Program.cs
+++ Day10_CathodeRayTube/CathodeRayTube/Program.cs
@@ -1,9 +1,40 @@
 ﻿namespace CathodeRayTube;
 
-internal class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string fileLocation = GetInputFilePath(@"\CathodeRayTube", "Day10Input.txt");
+
+        string[] input = File.ReadAllLines(fileLocation);
+
+        CPU cPU = new();
+        SignalStrengthMonitor signalStrengthMonitor = new();
+        cPU.ConnectMonitor(signalStrengthMonitor);
+
+        foreach (string line in input)
+        {
+            if (line == "noop") cPU.Noop();
+            else
+            {
+                int value = int.Parse(line.Substring(5));
+                cPU.AddX(value);
+            }
+        }
+
+        Console.WriteLine(signalStrengthMonitor.SignalStrength);
+    }
+
+    public static string GetInputFilePath(string parentDirName, string fileName)
+    {
+        string currentDirectory = Directory.GetCurrentDirectory();
+        int parentDirIndex = currentDirectory.LastIndexOf(parentDirName);
+        int lengthOfParentDirPath = parentDirName.Length + parentDirIndex;
+        string fullParentPath = currentDirectory.Substring(0, lengthOfParentDirPath);
+
+        string filePath = fullParentPath + @"\" + fileName;
+
+        if (File.Exists(filePath)) return filePath;
+        else throw new FileNotFoundException();
     }
 }
---
+++ Day10_CathodeRayTube/CathodeRayTubeTests/ProgramTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CathodeRayTube;
+
+namespace CathodeRayTubeTests;
+
+internal class ProgramTests
+{
+    [Test]
+    public void GivenAFileAndParent_GetInputFilePath_ReturnsCorrectPath()
+    {
+        string expected = "C:\\Users\\Spore\\source\\repos\\AdventOfCode2022\\Day10_CathodeRayTube\\CathodeRayTubeTests\\ProgramTests.cs";
+        string parentDir = @"\CathodeRayTubeTests";
+        string fileName = "ProgramTests.cs";
+
+        string actual = Program.GetInputFilePath(parentDir, fileName);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
