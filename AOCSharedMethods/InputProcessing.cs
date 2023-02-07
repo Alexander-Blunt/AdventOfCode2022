8a5539ddc66c85551f5e2743f7254c2b2bd2c411
Alexander Blunt <55701570+Alexander-Blunt@users.noreply.github.com>
2023-02-07 10:41:16 +0000

    Added comments for input Processing

--- AOCSharedMethods/InputProcessing.cs
+++ AOCSharedMethods/InputProcessing.cs
@@ -2,6 +2,13 @@
 
 public class InputProcessing
 {
+    /// <summary>
+    /// Finds the location of the input file using the name of the file's parent directory and the name of the file.
+    /// </summary>
+    /// <param name="parentDirName">The name of the file's parent directory</param>
+    /// <param name="fileName">The file name</param>
+    /// <returns></returns>
+    /// <exception cref="FileNotFoundException"></exception>
     public static string GetInputFilePath(string parentDirName, string fileName)
     {
         string currentDirectory = Directory.GetCurrentDirectory();
