6ebe4e8dfdfb2e43c9d2cd7d09e1d06fc5e96e23
Alexander Blunt <55701570+Alexander-Blunt@users.noreply.github.com>
2023-02-22 15:00:54 +0000

    Day 13 initial commit

---
+++ Day13_DistressSignal/DistressSignalTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DistressSignalTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
